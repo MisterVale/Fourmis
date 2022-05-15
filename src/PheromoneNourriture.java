dfb1c38e9e437953a99372394a8c182b74dbef20
MisterVale <88310477+MisterVale@users.noreply.github.com>
2022-05-15 16:09:10 +0200

    Ajout de commentaires

--- src/PheromoneNourriture.java
+++ src/PheromoneNourriture.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 class PheromoneNourriture extends Pheromone
 {
+	// Cree des pheromones lors de la recuperation de ressources avant de les dessiner sur le jeu 
     public PheromoneNourriture(int x, int y, Fourmiliere f, Color c)
     {
 	super(x,y,f);
