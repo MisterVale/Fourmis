b9df045fea354c9178ca03c31141bdf7af4f19a7
MisterVale <88310477+MisterVale@users.noreply.github.com>
2022-05-15 16:08:45 +0200

    Ajout de commentaires

--- src/Placement.java
+++ src/Placement.java
@@ -4,8 +4,8 @@ public class Placement extends Exception
     public Object obj;
     public Pheromone phe; //Pour les fourmis
     public boolean live;
-    public int old_x;
-    public int old_y;
-    public int new_x;
-    public int new_y;
+    public int old_x; //Ancienne position x
+    public int old_y; //Ancienne position y
+    public int new_x; //Nouvelle position x
+    public int new_y; //Nouvelle position y
 }
